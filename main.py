b8d67e601fdc3788fc2c909054a812ee97d6b1fa
vivek788more <30067947-vivek788more@users.noreply.replit.com>
2023-12-06 07:06:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print ("--- Username Generator for Cricket Teams ---\n")
+
+print("----------------------------------------------------\n")
+
+city = input("Enter the City you belongs to: ")
+
+moviename = input ("\nEnter your favourite movie name: ")
+
+
+print("\nYour Cricket Team Name is: " + city + " " + moviename)
